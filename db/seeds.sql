a89acac7f7e5d3534da425ab73de33a861930dcd
Alan <andrewsalan88@gmail.com>
2018-07-14 13:10:27 -0500

    seeds, schema, started on server

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bleu cheese burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("hatch chile burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("double bacon burger", false);
