477c535b4352cdf38d8e44c2325a837c0cddbf49
Alan <andrewsalan88@gmail.com>
2018-07-14 15:06:47 -0500

    made changes to the server.js and created a connection.js file

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+  
--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 var express = require("express");
 var bodyparser = require("body-parser");
+var exphbs = require("express-handlebars");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var app = express();
