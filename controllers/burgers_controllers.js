3ea2ab1d12b60dd98b4616eedb4d3753d1a8f0e0
Alan <andrewsalan88@gmail.com>
2018-07-14 19:37:15 -0500

    aghghgh

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -14,14 +14,14 @@ router.get("/", function(req, res) {
     });
   });
   
-  router.post("/api/burgers", function(req, res) {
+  router.post("/", function(req, res) {
     burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
   
-  router.put("/api/burgers/:id", function(req, res) {
+  router.put("/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("devoured", condition);
